34fdf500abdc8b91f6186955ac80c1208eaf9b49
Liam McHara <guillemlr@proton.me>
2024-03-11 19:09:50 +0100

    handleUpdate s

--- netlify/functions/update.ts
+++ netlify/functions/update.ts
@@ -1,10 +1,13 @@
 import { run } from "../..";
+import { bot } from "../../src/globals";
+
+run();
 
 exports.handler = async (event) => {
 	const logMsg = `Received an update from Telegram! : ${event.body}`;
 	console.log(logMsg);
+	bot.handleUpdate(JSON.parse(event.body));
 	try {
-		run();
 		return { statusCode: 200, body: logMsg };
 	} catch (error) {
 		console.error(error);
